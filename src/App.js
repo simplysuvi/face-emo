a48f700c3a1c0ad4d6c95eb4aad3d60bad341ba9
Suvrat Jain <suvijain@gmail.com>
2020-04-08 14:23:27 +0530

    .

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ function App() {
         <div className="App__header">
           <h1>
             <span>Mood Detector</span>
-            <br><p>MADE WITH LOVE BY SUVRAT JAIN</p>
           </h1>
           <div className="App__switcher">
             <FontAwesomeIcon
